809fdefc4d1b2983f43bf2cb6c2bb2aebb8860d7
jyoung0930 <jaronyoung1@gmail.com>
2022-08-10 08:18:32 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,31 +1,14 @@
-const colorWheelSelection = document.querySelector("#favcolor");
-const colorDropdownSelection = document.querySelector("#color");
-const colorSection = document.querySelectorAll(".color-section");
-const hexSection = document.querySelectorAll(".hex-section");
-const form = document.querySelector("form");
+import req from "./req.js";
 const URL = "https://www.thecolorapi.com/scheme";
+const form = document.querySelector("form");
 
+// Load initial colors from inputs
+req(URL);
+
+// Get input colors on submit
 form.addEventListener("submit", (e) => {
   e.preventDefault();
-  const color = colorWheelSelection.value.slice(1);
-  const scheme = colorDropdownSelection.value.toLowerCase();
-  const colorArray = [];
-  const hexSectionArray = Array.from(hexSection);
-  const colorSectionArray = Array.from(colorSection);
-  fetch(URL + `?hex=${color}&mode=${scheme}&count=5`)
-    .then((res) => res.json())
-    .then((data) => {
-      const colorData = data.colors;
-      for (let i = 0; i < colorData.length; i++) {
-        colorArray.push(colorData[i].hex.value);
-      }
-      for (let i = 0; i < hexSectionArray.length; i++) {
-        hexSectionArray[i].textContent = colorArray[i];
-        colorSectionArray[i].style.backgroundColor = colorArray[i];
-      }
-
-      console.log(colorArray);
-      console.log(typeof hexSection);
-    });
+  req(URL);
 });
+
 
